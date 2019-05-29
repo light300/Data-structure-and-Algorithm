80567cf454c7032e9fb1ddbc88c29f700983437f
Hung En Hsieh <m1029003@astro.ncu.edu.tw>
2019-05-29 23:20:44 +0800

    Add delete AVL node

--- search/AVL.c
+++ search/AVL.c
@@ -143,6 +143,62 @@ void Inorder(struct Node *p)
 	}
 }
 
+struct Node *Inpre(struct Node *p)
+{
+    while(p && p->rchild!=NULL)
+        p = p->rchild;
+    return p;
+}
+
+struct Node *Insucc(struct Node *p)
+{
+    while(p && p->lchild!=NULL)
+        p = p->lchild;
+    return p;
+}
+
+struct Node *Delete(struct Node *p, int key)
+{
+    struct Node *q;
+    
+    if (p == NULL)
+        return NULL;
+    if (p->lchild == NULL && p->rchild == NULL){
+        if (p == root)
+            root = NULL;
+        free(p);
+        return NULL;
+    }
+    if (key < p->data)
+        p->lchild = Delete(p->lchild, key);
+    else if (key > p->data)
+        p->rchild = Delete(p->rchild, key);
+    else {
+        if (p->lchild->heigh > p->rchild->heigh) {
+            q = Inpre(p->lchild);
+            p->data = q->data;
+            p->lchild = Delete(p->lchild, key);
+        } else {
+            q = Insucc(p->rchild);
+            p->data = q->data;
+            p->rchild = Delete(p->rchild, key);
+        }
+    }
+    
+    p->heigh = Nodeheigh(p);
+    
+    if (BalanceFactor(p)==2 && BalanceFactor(p->lchild)==1)
+        return LLrotation(p);
+    else if (BalanceFactor(p)==2 && BalanceFactor(p->lchild)==-1)
+        return LRrotation(p);
+    else if (BalanceFactor(p)==-2 && BalanceFactor(p->rchild)==-1)
+        return RRrotation(p);
+    else if (BalanceFactor(p)==-2 && BalanceFactor(p->rchild)==1)
+        return RLrotation(p);
+    
+    return p;
+}
+
 int main()
 {
 	root = Rinsert(root, 10);
@@ -151,6 +207,8 @@ int main()
 	Rinsert(root, 25);
 	Rinsert(root, 28);
 	Rinsert(root, 27);
+
+	Delete(root, 25);
 
 	Inorder(root);
 	printf("\n");
